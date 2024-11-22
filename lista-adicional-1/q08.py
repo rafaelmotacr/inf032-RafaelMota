cb6d2b4f2ff2fd4f624972e42d13e7a8ce94fec1
rafaelmotacr <rafaelmota361@gmail.com>
2024-11-22 10:32:23 -0300

    Questão 8 da lista adicional concluida.

--- lista-adicional-1/q08.py
+++ lista-adicional-1/q08.py
@@ -1,61 +1,59 @@
-import pandas as pd
+import pandas as pd 
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-# Ler o arquivo Excel usando a biblioteca openpyxl
-try:
-    df = pd.read_excel('Untitled 1.xls', sheet_name='Plan1', engine='openpyxl')
-except FileNotFoundError:
-    print("O arquivo 'Untitled 1.xls' não foi encontrado. Verifique o nome e o caminho.")
-    exit()
 
-# Garantir que as colunas 'A' e 'B' sejam lidas corretamente como float
-try:
-    vale3 = df['A'].astype(float).values
-    ggbr4 = df['B'].astype(float).values
-except KeyError:
-    print("Certifique-se de que o arquivo possui colunas 'A' e 'B'.")
-    exit()
+excel = pd.read_excel('Dados_vale__Gerdau.xlsx', sheet_name='Plan1', header=None)
 
-# Calcular os retornos diários
-retorno_vale = (vale3[1:] - vale3[:-1]) / vale3[:-1]
-retorno_ggbr = (ggbr4[1:] - ggbr4[:-1]) / ggbr4[:-1]
 
-# Criar a figura com 2x2 subplots
-fig, axs = plt.subplots(2, 2, figsize=(12, 10))
+excel[0] = pd.to_numeric(excel[0], errors='coerce')
+excel[1] = pd.to_numeric(excel[1], errors='coerce')
 
-# Gráfico do preço da Vale na parte superior
-axs[0, 0].plot(vale3, label='Preço Vale3', color='blue')
-axs[0, 0].set_title('Preço da Vale (VALE3)')
-axs[0, 0].set_xlabel('Dia')
-axs[0, 0].set_ylabel('Preço')
-axs[0, 0].legend()
-axs[0, 0].grid(True)
 
-# Gráfico do retorno da Vale na parte superior
-axs[0, 1].plot(retorno_vale, label='Retorno Vale3', color='blue')
-axs[0, 1].set_title('Retorno da Vale (VALE3)')
-axs[0, 1].set_xlabel('Dia')
-axs[0, 1].set_ylabel('Retorno')
-axs[0, 1].legend()
-axs[0, 1].grid(True)
+excel.dropna(inplace=True)
 
-# Gráfico do preço da Gerdau na parte inferior
-axs[1, 0].plot(ggbr4, label='Preço GGBR4', color='red')
-axs[1, 0].set_title('Preço da Gerdau (GGBR4)')
-axs[1, 0].set_xlabel('Dia')
-axs[1, 0].set_ylabel('Preço')
-axs[1, 0].legend()
-axs[1, 0].grid(True)
 
-# Gráfico do retorno da Gerdau na parte inferior
-axs[1, 1].plot(retorno_ggbr, label='Retorno GGBR4', color='red')
-axs[1, 1].set_title('Retorno da Gerdau (GGBR4)')
-axs[1, 1].set_xlabel('Dia')
-axs[1, 1].set_ylabel('Retorno')
-axs[1, 1].legend()
-axs[1, 1].grid(True)
+vale = np.array(excel[0])
+gerdau = np.array(excel[1])
 
-# Ajustar o layout e mostrar os gráficos
-plt.tight_layout()
+
+dias = np.arange(0, len(vale))
+
+plt.subplot(211)
+plt.plot(dias, vale)
+plt.title('Preços da Vale')
+
+plt.subplot(212)
+plt.plot(dias, gerdau)
+plt.title('Preços da Gerdau')
+plt.show()
+
+
+ret_vale = (vale[1:] - vale[:-1]) / vale[:-1]
+ret_gerdau = (gerdau[1:] - gerdau[:-1]) / gerdau[:-1]
+
+
+dias_ret = np.arange(0, len(ret_vale))
+
+
+plt.figure(figsize=(10, 8))
+plt.subplot(221)
+plt.plot(dias, vale)
+plt.title('Preço da Vale')
+
+plt.subplot(222)
+plt.plot(dias, gerdau)
+plt.title('Preço da Gerdau')
+
+plt.subplot(223)
+plt.plot(dias_ret, ret_vale)
+plt.title('Retorno da Vale')
+
+plt.subplot(224)
+plt.plot(dias_ret, ret_gerdau)
+plt.title('Retorno da Gerdau')
+
+plt.tight_layout()  
 plt.show()
